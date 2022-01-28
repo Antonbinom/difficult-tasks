115706334955fa0a0a11a183890c0031392a55df
Anton Semenikhin <protest_the_hero@mail.ru>
2022-01-28 20:37:59 +0300

    lesson 2 complete

--- main.js
+++ main.js
@@ -1,13 +1,33 @@
+// способ первый
 let num = 266219;
-let numArr;
+let numArr = String(num).split('');
+let count = 1;
+numArr.forEach(function (elem) {
+	count *= elem;
+});
+console.log(count);
 
-function multiply() {
-	numArr = String(num).split('');
-	let n = 1;
-	for (i = 0; i < numArr.length; i++) {
-		n *= numArr[i];
-	}
-	console.log(n);
-}
+let countTripled = count * 3;
 
-multiply();
+console.log(countTripled);
+console.log(countTripled.toString().slice(0, 2));
+
+
+// способ второй
+// let num = 266219;
+
+// function multiply() {
+// 	numArr = String(num).split('');
+// 	let count = 1;
+// 	for (i = 0; i < numArr.length; i++) {
+// 		count *= numArr[i];
+// 	}
+// 	console.log(count);
+
+// 	let countTripled = count * 3;
+// 	console.log(countTripled);
+
+// 	console.log(countTripled.toString().slice(0, 2));
+// }
+
+// multiply();
