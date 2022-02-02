12c0c3780e7e46c735b504f4c3a22ed71b3a07ca
Anton Semenikhin <protest_the_hero@mail.ru>
2022-02-03 00:39:57 +0300

    cut code

--- main.js
+++ main.js
@@ -1,15 +1,9 @@
 'use strict';
 
-const isNumber = function (num) {
-	return !isNaN(parseFloat(num) && isFinite(num));
-};
-
-while (!isNumber(num)) {
+while (isNaN(parseFloat(num) && isFinite(num))) {
 	var num = prompt('Введите число', '');
 	if (num == null) break;
 	num = num.trim().split(' ')[0].replace(/[^+\d]/g, '');
 }
-
-isNumber();
 
 console.log(num);
