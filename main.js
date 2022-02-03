8ad6f3a093364a0e9dabe668e49251ade87db957
Anton Semenikhin <protest_the_hero@mail.ru>
2022-02-03 09:29:09 +0300

    fix

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 'use strict';
 
 let arr = ['10', '23', '41', '2', '4', '14', '420', '200', '344', '142', '42', '24'];
-let newArr = arr.filter(num => num[0] == '2' || num[0] == '4');
+
+const newArr = arr.filter(function (num) {
+	return num[0] == '2' || num[0] == '4';
+});
 
 console.log(newArr);
 
 let n = 100;
 
-nextPrime:
+loop:
 	for (let i = 0; i <= n; i++) {
 
 		for (let n = 2; n < i; n++) {
-			if (i % n == 0) continue nextPrime;
+			if (i % n == 0) continue loop;
 		}
 
-		console.log(i + ' делитится на ' + 1 + ' и на ' + i);
+		console.log(i + ' делится на ' + 1 + ' и на ' + i);
 	}
