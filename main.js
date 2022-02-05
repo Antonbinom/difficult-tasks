0527678319505e56b0f92ddf4ccdabb279eb4aed
Anton Semenikhin <protest_the_hero@mail.ru>
2022-02-05 16:53:35 +0300

    Lesson 7 - is ready

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 'use strict';
 
-/**
-1) Создать массив week и записать в него дни недели в виде строк
-Вывести на экран все дни недели
-Каждый из них с новой строчки
-Выходные дни - курсивом
-Текущий день - жирным шрифтом(использовать объект даты)
- */
+const week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-const week = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'saunday'];
+const div = document.getElementById('div');
 
-function array(item, index) {
-	for (let key of week) {
-		return key;
+const now = new Date().getDay();
 
+week.forEach(function (item, index) {
+	let p = document.createElement('p');
+	p.innerHTML = item;
+	if (index == 5 || index == 6) {
+		p.style.fontStyle = "Italic";
 	}
-}
+	if (index == new Date().getDay() - 1) {
+		p.style.color = "red";
+	}
+	div.appendChild(p);
+});
