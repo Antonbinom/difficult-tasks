cbebc3cd88ab2ff645b5b428df0ab48d28516d9b
Anton Semenikhin <protest_the_hero@mail.ru>
2022-02-14 22:08:23 +0300

    lesson 13 homework in process

--- js/main.js
+++ js/main.js
@@ -5,45 +5,80 @@ const headerInput = document.querySelector('.header-input'),
 	todoControl = document.querySelector('.todo-control'),
 	todoCompleted = document.querySelector('.todo-completed');
 
-const todoData = [];
+const todoData = []; // создаем массив куда будем добавлять новые тудухи
 
 const render = function () {
-	todoList.innerHTML = '';
+	todoList.innerHTML = ''; // при добавлении новой toDo
 	todoCompleted.innerHTML = '';
 
-	todoData.forEach(function (item) {
-		const li = document.createElement('li');
+	todoData.forEach(function (item, index) { // перебираем массив toDo
+		const li = document.createElement('li'); // создаем элемент li
 
+		// с классом
 		li.classList.add('todo-item');
-		li.innerHTML = '<span class="text-todo">' + item.text + '</span>' +
+		// и версткой внутри
+		li.innerHTML = '<span class="text-todo">' + item.text + '</span>' + //
 			'<div class="todo-buttons">' +
 			'<button class="todo-remove"></button>' +
 			'<button class="todo-complete"></button>' +
 			'</div>';
+		// если у объекта свойство complited == true
 		if (item.completed) {
-			todoCompleted.append(li);
+			todoCompleted.append(li); // добавляем в завершенные
+			console.log(todoData)
+
 		} else {
-			todoList.append(li);
+			todoList.append(li); // иначе в незавершенных
+			console.log()
+
 		}
 
+		localStorage.setItem(todoData, JSON.stringify(todoData));
+
+		console.log(typeof todoData); // объект
+		console.log(todoData); // [1, 2, 3]
+
+
+
+
+
+		// localStorage.setItem('text' + index, item.text);
+
+		// при нажатии на иконку с галочкой
 		li.querySelector('.todo-complete').addEventListener('click', function () {
-			item.completed = !item.completed;
+			item.completed = !item.completed; // изменяем значение у свойства completed на true/false
 			render();
+		});
+
+		//при нажатии на корзину ужаляем дело
+		li.querySelector('.todo-remove').addEventListener('click', function () {
+			li.remove();
+			todoData.splice(index, 1);
+			localStorage.removeItem(index, item.text);
+			console.log(index);
 		});
 	});
 };
 
+// при нажатии на кнопку +
 todoControl.addEventListener('submit', function (event) {
 	event.preventDefault();
-	// const li = document.createElement
 
-	const newTodo = {
-		text: headerInput.value,
-		completed: false
-	};
+	if (headerInput.value == '') {
+		headerInput.placeholder = 'Введите название задачи!';
 
-	todoData.push(newTodo);
-	headerInput.value = '';
+	} else {
+		const newTodo = { // создаем новый объект  со свойствами
+			text: headerInput.value, // значение берем из инпута
+			completed: false
+		};
 
-	render();
+		todoData.push(newTodo); // и вставляем элемент в массив
+		headerInput.value = ''; // очищаем инпут
+
+		render(); // запускаем функцию где создается новый элемент
+		headerInput.placeholder = 'Какие планы?';
+	}
 });
+
+localStorage.removeItem('text0');
