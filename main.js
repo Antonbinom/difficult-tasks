aa07c3c662953cdaef07b3d4c1c3a906a54c7e51
Anton Semenikhin <protest_the_hero@mail.ru>
2022-03-10 09:29:16 +0300

    ready

--- main.js
+++ main.js
@@ -1,82 +1,40 @@
 'use strict';
 
-// const getData = (url) => { // функция получения данных из файла db.json
-// 	fetch(url)
-// 		.then(response => response.json()) // получаем данные и обрабатываем их json
-// 		.then(data => {
-// 			console.log(data);
-// 			sendData({
-// 					url: 'https://jsonplaceholder.typicode.com/posts',
-// 					dataBase: JSON.stringify(data),
-// 					method: 'POST'
-// 				})
-// 				.then(data => console.log(data))
-// 				.catch(error => console.log(error));
-// 		}) // запускаем функцию отправки данных и передаем в нее полученные данные
-// 		.catch(error => {
-// 			console.log(error);
-// 		});
-// }
+const requestURL = 'https://jsonplaceholder.typicode.com/posts';
 
-// const sendData = ({
-// 	url,
-// 	dataBase,
-// 	method = 'GET'
-// }) => { // функция отправки данных
-// 	return fetch(url, { // отправляь будем на данны url
-// 		method: method, // метод отправки
-// 		body: dataBase, // преобразуем данные
-// 		headers: {
-// 			'Content-type': 'application/json; charset=UTF-8', // указываем тип данных
-// 		},
-// 	}).then(response => response.json())
-// };
+const sendData = ((method, url, body = null) => {
+	return new Promise((resolve, reject) => {
+		const xhr = new XMLHttpRequest();
 
-// getData('db.json');
+		xhr.open(method, url);
 
-const getData = (url) => {
-	const xhr = new XMLHttpRequest();
-	xhr.open(
-		'GET',
-		url,
-		true
-	)
-	xhr.send();
+		xhr.responseType = 'json';
+		xhr.setRequestHeader('Content-Type', 'application/json');
 
-	xhr.onreadystatechange = function () {
-		if (xhr.readyState != 4) {
-			return;
+		xhr.onload = () => {
+			if (xhr.status >= 400) {
+				reject(xhr.response)
+			} else {
+				resolve(xhr.response)
+			}
 		}
-
-		if (xhr.status === 200) {
-			console.log(JSON.parse(xhr.responseText));
-			sendData({
-				url: 'https://jsonplaceholder.typicode.com/posts',
-				dataBase: JSON.parse(xhr.responseText),
-				method: 'POST'
-			});
-		} else {
-			console.log('error', xhr.responseText);
+		xhr.onerror = () => {
+			reject(xhr.response)
 		}
-	};
+		xhr.send(JSON.stringify(body));
+	});
+});
 
+const body = {
+	"user": "Vlad",
+	"age": 33,
+	"role": "developer"
 };
 
-const sendData = ({
-	url,
-	dataBase,
-	method = 'GET'
-}) => {
-	const xhr = new XMLHttpRequest();
-	xhr.open(method, url, true); // определяем параметры для запроса
-	xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded'); // задаем значение заголовка HTTP запроса (указываем, что передаваемое значение на сервер кодируется в кортежах с ключом, разделенных символоми '&', с '=' между ключом и значением)
-	xhr.onreadystatechange = function () { //Call a function when the state changes.
-		if (xhr.readyState == 4 && xhr.status == 200) {
-			alert(xhr.responseText);
-		}
-	}
-	xhr.send(dataBase); // отправляем запрос на сервер
-	console.log(JSON.parse(dataBase))
-};
+sendData("GET", '/db.json', true)
+	.then(data => console.log(data))
+	.catch(err => console.log(err));
 
-getData('/db.json');
+sendData('POST', requestURL, body)
+	.then(data => console.log(data))
+	.catch(err => console.log(err));
