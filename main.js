5ab2a5f4483a9c6bf54fd545d8d3e75e82d3f34e
Anton Semenikhin <protest_the_hero@mail.ru>
2022-03-10 08:52:23 +0300

    lesson28

--- main.js
+++ main.js
@@ -4,40 +4,64 @@ const select = document.querySelector('select');
 const options = select.querySelectorAll('option');
 
 const getData = () => {
-	fetch('cars.json')
+	fetch('/cars.json')
 		.then(response => response.json())
-		.then(data => {
-			console.log(data);
-			data.forEach((element, i) => {
-				console.log(element[i].brand);
-			});
-			sendData({
-					url: 'https://jsonplaceholder.typicode.com/posts',
-					dataBase: JSON.stringify(data),
-					method: 'POST'
-				})
-				.then(data => {
-					console.log(data);
-				})
-				.catch(error => console.log(error));
-		})
-		.catch(error => {
-			console.log(error);
-		});
-}
+		.then(data => console.log(data));
+};
 
-const sendData = ({
-	url,
-	dataBase,
-	method = 'GET'
-}) => { // функция отправки данных
-	return fetch(url, { // отправляь будем на данны url
-		method: method, // метод отправки
-		body: dataBase, // преобразуем данные
+const sendData = (data) => {
+	fetch('https://jsonplaceholder.typicode.com/posts', {
+		method: 'POST',
+		body: JSON.stringify(data),
 		headers: {
-			'Content-type': 'application/json; charset=UTF-8', // указываем тип данных
-		},
-	}).then(response => response.json())
+			'Content-Type': 'application/json'
+		}
+	}).then(res => res.json());
+
+
 };
 
 getData();
+
+
+
+
+
+// const getData = () => {
+// 	fetch('cars.json')
+// 		.then(response => response.json())
+// 		.then(data => {
+// 			console.log(data);
+// 			data.forEach((element, i) => {
+// 				console.log(element[i].brand);
+// 			});
+// 			sendData({
+// 					url: 'https://jsonplaceholder.typicode.com/posts',
+// 					dataBase: JSON.stringify(data),
+// 					method: 'POST'
+// 				})
+// 				.then(data => {
+// 					console.log(data);
+// 				})
+// 				.catch(error => console.log(error));
+// 		})
+// 		.catch(error => {
+// 			console.log(error);
+// 		});
+// }
+
+// const sendData = ({
+// 	url,
+// 	dataBase,
+// 	method = 'GET'
+// }) => { // функция отправки данных
+// 	return fetch(url, { // отправляь будем на данны url
+// 		method: method, // метод отправки
+// 		body: dataBase, // преобразуем данные
+// 		headers: {
+// 			'Content-type': 'application/json; charset=UTF-8', // указываем тип данных
+// 		},
+// 	}).then(response => response.json())
+// };
+
+// getData();
